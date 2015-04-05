ea334f6ea55b9fb820fa8ba8e4d0b847e03e50de
jeansfelix <jean.s.felix@gmail.com>
2015-04-05 16:46:44 -0300

    Criando um modo de enviar mensagens do cliente para o servidor.

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,15 +1,30 @@
 package cliente;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente
 {
 
     public static void main(String[] args) throws IOException
     {
-        Socket cliente = new Socket("127.0.0.1",7777);
+        Socket cliente = new Socket("127.0.0.1", 7777);
         System.out.println("O cliente se conectou ao servidor!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String mensagem = "";
+
+        while (!"#".equals(mensagem))
+        {
+            PrintStream printStream = new PrintStream(cliente.getOutputStream());
+
+            mensagem = scanner.nextLine();
+
+            printStream.println(mensagem);
+        }
 
         cliente.close();
     }
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,6 +1,8 @@
 package servidor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,6 +18,18 @@ public class Servidor
             Socket cliente = servidor.accept();
 
             System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
+            
+            String mensagemLida = "";
+            
+            while (!"#".equals(mensagemLida))
+            {
+                InputStreamReader reader = new InputStreamReader(cliente.getInputStream());
+                BufferedReader leitor = new BufferedReader(reader);
+                
+                mensagemLida = leitor.readLine();
+                
+                System.out.println(mensagemLida);
+            }
         }
 
     }
